f36a104467031b209efd1d85c0120a1c50d51db6
LenyPython <buniadyserv@gmail.com>
2021-09-22 16:35:59 +0200

    simplifying new array generation

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,20 @@ import * as d3 from 'd3'
 
 function App() {
   const [data, setData] = useState<number[]>([])
-  const md = useRef()
-  const svgRef = useRef()
+  const svgRef = useRef(null)
   const generateArray = (): number[] => new Array(20).fill(1).map((item, idx)=>Math.floor(Math.random()*idx+item))
 
   useEffect(():void => {
     setData(generateArray())
   }, [])
 
-  useEffect((): void =>{
-  md.current = data.map((item, idx) =>  <p key={idx}>{item}</p>)
-  },[data])
   const handleClick = (): void => setData(generateArray())
 
   return (
     <div className="App">
       <svg ref={svgRef}></svg>
       <button onClick={handleClick}>Generate new array</button>
-      {md.current}
+      {data}
     </div>
   );
 }
